0c52f3b6c3a2212674d0135f010f1c6ddc65a501
WonderfulWan8 <fudengweiw@gmail.com>
2022-04-02 16:05:04 +0800

    yibu1

--- 2_js/05_异步和单线程/01_setTimeout.js
+++ 2_js/05_异步和单线程/01_setTimeout.js
@@ -10,10 +10,22 @@
 // 1 3 5 4 2
 console.log("===================================");
 
-// 异步
+// 异步 (callback 回调函数)
+// 异步不会阻塞后面的函数
 console.log(100);
 setTimeout( function(){
     console.log(200);
 })
 console.log(300);
 // 100 300 200
+
+// 同步
+console.log(100);
+alert(200);
+console.log(300);
+// 100 200 300
+
+
+// 异步应用场景
+//  网络请求，如ajax图片加载
+//  定时任务，如setTimeout()
